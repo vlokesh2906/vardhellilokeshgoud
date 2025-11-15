9ad1f527ec9b20f0a9e0de058df55557a57c6c4c
Lokesh Goud Vardhelli <vardhellilokeshgoud96@gmail.com>
2025-11-15 07:51:57 +0530

    Update Task3.py

--- AAIP_ASSIGNMENTS/Assignment-1/Task3.py
+++ AAIP_ASSIGNMENTS/Assignment-1/Task3.py
@@ -1,1 +1,8 @@
+def reverse_string(s):
+    """Return the reversed string."""
+    return s[::-1]
 
+# Take user input
+user_input = input("Enter a string to reverse: ")
+reversed_str = reverse_string(user_input)
+print(f"Reversed string: {reversed_str}")
