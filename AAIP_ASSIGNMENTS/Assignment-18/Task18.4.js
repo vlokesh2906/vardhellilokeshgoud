89c75119d904cbe956b5c7d71dcdf009f6cea13d
Lokesh Goud Vardhelli <vardhellilokeshgoud96@gmail.com>
2025-12-08 18:07:59 +0530

    Create Task18.4.js

---
+++ AAIP_ASSIGNMENTS/Assignment-18/Task18.4.js
@@ -0,0 +1,8 @@
+function printStudents(students) {
+    console.log("Student List:");
+    for (let name of students) {
+        console.log(name);
+    }
+}
+
+printStudents(["Alice", "Bob", "Charlie"]);
