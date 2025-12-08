cf0a1076a06ebc6fff10ab73fd9274b58ea659e3
Lokesh Goud Vardhelli <vardhellilokeshgoud96@gmail.com>
2025-12-08 17:59:41 +0530

    Create Task18.2.java

---
+++ AAIP_ASSIGNMENTS/Assignment-18/Task18.2.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void checkNumber(int num) {
+        if (num > 0)
+            System.out.println("The number is positive");
+        else if (num < 0)
+            System.out.println("The number is negative");
+        else
+            System.out.println("The number is zero");
+    }
+
+    public static void main(String[] args) {
+        checkNumber(-5);
+        checkNumber(0);
+        checkNumber(7);
+    }
+}
