857dbb87a22a06bf936d63cd0dd25b2fdbc64acc
Lokesh Goud Vardhelli <vardhellilokeshgoud96@gmail.com>
2025-12-08 18:00:25 +0530

    Create Task18.2.py

---
+++ AAIP_ASSIGNMENTS/Assignment-18/Task18.2.py
@@ -0,0 +1,12 @@
+#TASK 18.2
+def check_number(num):
+    if num > 0:
+        print("The number is positive")
+    elif num < 0:
+        print("The number is negative")
+    else:
+        print("The number is zero")
+
+check_number(-5)
+check_number(0)
+check_number(7)
